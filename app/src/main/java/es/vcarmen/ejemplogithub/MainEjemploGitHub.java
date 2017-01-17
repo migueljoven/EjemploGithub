c1e0a51c38a0520e3abaf4e7d3d20250741a025e
mañana <maana>
2017-01-17 13:28:49 +0100

    Initial commit

---
+++ app/src/main/java/es/vcarmen/ejemplogithub/MainEjemploGitHub.java
@@ -0,0 +1,13 @@
+package es.vcarmen.ejemplogithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainEjemploGitHub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_ejemplo_git_hub);
+    }
+}

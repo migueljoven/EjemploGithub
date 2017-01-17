31de7085e552e94bc5010272ec949f422c7c96b4
migueljoven <migueljoven@gmail.com>
2017-01-17 13:34:30 +0100

    Boton nuevo

--- app/src/main/java/es/vcarmen/ejemplogithub/MainEjemploGitHub.java
+++ app/src/main/java/es/vcarmen/ejemplogithub/MainEjemploGitHub.java
@@ -2,6 +2,7 @@ package es.vcarmen.ejemplogithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainEjemploGitHub extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainEjemploGitHub extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_ejemplo_git_hub);
+
+        Button boton;
     }
 }
